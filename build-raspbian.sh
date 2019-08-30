4d38b286effb524933b919824aa503b745a3501a
Alexey Rogachevskiy <sfalexrog@gmail.com>
2019-08-30 21:22:48 +0300

    travis: Build for multiple releases

--- build-raspbian.sh
+++ build-raspbian.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
-RPI_IMAGE=https://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2019-07-12/2019-07-10-raspbian-buster-lite.zip
+RPI_BASE_URL=https://downloads.raspberrypi.org/raspbian_lite/images
+RPI_PREFIX=raspbian_lite
+
+# Raspbian release dates are somewhat inconsistent
+
+case "${RELEASE_DATE}" in
+    "2019-07-12")
+        RPI_IMAGE_NAME="2019-07-10-raspbian-buster-lite"
+        ;;
+    "2019-04-09")
+        RPI_IMAGE_NAME="2019-04-08-raspbian-stretch-lite"
+        ;;
+    *)
+        echo "Unsupported release date: ${RELEASE_DATE}"
+        exit 1
+esac
+
+RPI_FOLDER="${RPI_PREFIX}-${RELEASE_DATE}"
+RPI_IMAGE="${RPI_BASE_URL}/${RPI_FOLDER}/${RPI_IMAGE_NAME}.zip"
 
 echo "Downloading image from ${RPI_IMAGE}"
 
-curl --location ${RPI_IMAGE} --fail --output image_archive.zip
+curl --location ${RPI_IMAGE} --fail --output "${RPI_IMAGE_NAME}.zip"
 
-unzip image_archive.zip
+unzip ${RPI_IMAGE_NAME}
 
-LODEV=$(losetup --find --read-only --partscan --show ./2019-07-10-raspbian-buster-lite.img)
+echo "Mounting ${RPI_IMG_FILE}"
+
+LODEV=$(losetup --find --read-only --partscan --show "${RPI_IMAGE_NAME}.img")
 
 echo "Image mounted to ${LODEV}"
 
@@ -24,7 +44,7 @@ mount -o ro ${LODEV}p2 ./rpi
 
 echo "Packing rootfs"
 
-tar -C ./rpi -czpf 2019-07-10-raspbian-buster-lite.tar.gz --numeric-owner .
+tar -C ./rpi -czpf "${RPI_IMAGE_NAME}.tar.gz" --numeric-owner .
 
 echo "Unmounting the image"
 
@@ -34,7 +54,6 @@ losetup -d ${LODEV}
 
 echo "Generating container"
 
-mv ./2019-07-10-raspbian-buster-lite.tar.gz docker-staging/
+mv "${RPI_IMAGE_NAME}.tar.gz" docker-staging/rootfs.tar.gz
 cd docker-staging
-docker build --rm --tag=sfalexrog/raspbian:buster-lite .
-
+docker build --rm --tag="sfalexrog/raspbian:${RELEASE_DIST}" .
